67b392ea0d435dc8537dfb5a3ee0bb674fb235d1
James Shea <jshea915@gmail.com>
2024-01-10 14:01:35 -0700

    boiler-plate setup. Webpack configured with necessary dev and build scripts. Server setup.

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+
+export function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+// This exports the entire file "App" or module.
+export default App;
