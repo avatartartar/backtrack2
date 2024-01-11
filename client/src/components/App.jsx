525d65ca97d8ec43388e850f8320fcc82a0b6706
Nina Skyttmo <ninaskyttmo@Ninas-MacBook-Pro-2.local>
2024-01-10 23:54:31 -0500

    nina-adding-mvp-style

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import Navbar from './Navbar.jsx';
+import SongList from './SongList.jsx';
+import TopAlbum from './TopAlbum.jsx';
 import '../../styles/index.scss';
 
 export function App() {
   return (
     <>
       <Navbar/>
+      <h3>Lets take a trip down memory lane</h3>
+      <h1 className="gradientHeader">This is your all time favorites</h1>
+      <div className="songListAndAlbum">
+      <SongList/>
+      <TopAlbum/>
+      </div>
     </>
   )
 }
