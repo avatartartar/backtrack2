dec61fb8e67cf8db529f5e3d1fdd4726d7a87f09
Keith Gibson <keith.r.gibson@gmail.com>
2024-01-12 19:22:14 -0800

    webAPIxSongPlaying

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -3,15 +3,18 @@ import model, { basicQueries } from '../models/model.js';
 // Controller object created to encapsulate functions related to handling requests to the DB or potentially api
 const controller = {};
 
-// Helper function that takes in a helper function 
+// Helper function that takes in a helper function
 const handleRequest = async (modelFunction, req, res) => {
   try {
     const data = await modelFunction();
     res.status(200).json(data);
+
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
+
+
 
 controller.getTop10Artists = (req, res) => handleRequest(basicQueries.getTop10Artists, req, res);
 controller.getTop10Albums = (req, res) => handleRequest(basicQueries.getTop10Albums, req, res);
