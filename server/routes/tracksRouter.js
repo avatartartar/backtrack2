004c68d202dc41dedf63caa85c528351481aa904
Keith Gibson <1214624+keithgibson@users.noreply.github.com>
2024-01-19 02:14:33 -0800

    also, the tracks list no longer re renders and the track list and track image elements dont move when a year is clicked. Not sure if its because of something I did or if it was already fixed when I pulled down dev most recently. Still getting bug of all of the tracks marqueeing when played, but only after a year is clicked ¯\_(ツ)_/¯

--- server/routes/tracksRouter.js
+++ server/routes/tracksRouter.js
@@ -3,14 +3,11 @@ const router = express.Router();
 import tracksController from '../controllers/tracksController.js';
 
 router.get('/', tracksController.getTopTracks, (req, res) => {
-  console.log('in tracksRouter get /');
   return res.status(200).json(res.locals.topTracks);
 });
-//Ross added this to set up a route for front end slider to get tracks by year.
+
 router.get('/ByYear', tracksController.getTopTracksByYear, (req, res) => {
-  console.log('in tracksRouter get /ByYear');
   return res.status(200).json(res.locals.topTracksByYear);
-  // return res.status(200).json(res.locals.TracksByTimePlayed);
 });
 
 router.get('/topTracksByYearByMonth', tracksController.getTopTracksByYearByMonth, (req, res) => {
