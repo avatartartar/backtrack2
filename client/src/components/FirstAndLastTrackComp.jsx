10904a43f3bd160316a87c3260aac83aba5d97e4
Trang <trang.do@chicagobooth.edu>
2024-02-02 16:37:00 -0500

    small tweaks

--- client/src/components/FirstAndLastTrackComp.jsx
+++ client/src/components/FirstAndLastTrackComp.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { useData } from './DataContext.jsx';
-import dexdb from './dexdb.js';
 
 
 
